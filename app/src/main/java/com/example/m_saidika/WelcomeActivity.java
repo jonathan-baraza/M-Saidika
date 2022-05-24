7d14fe600b5d009147741a429dfa6f01eaf047d0
Jonathan Baraza <jbaraza46@gmail.com>
2022-05-24 18:03:57 +0300

    Created Login activity

--- app/src/main/java/com/example/m_saidika/WelcomeActivity.java
+++ app/src/main/java/com/example/m_saidika/WelcomeActivity.java
@@ -9,17 +9,18 @@ import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
     public Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        btn=findViewById(R.id.btnProceed);
+        btn=findViewById(R.id.btnLogin);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(WelcomeActivity.this,MainActivity.class);
+                Intent intent=new Intent(WelcomeActivity.this,LoginActivity.class);
                 startActivity(intent);
             }
         });
