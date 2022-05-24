79486fe4e0bbed76fc7097f936a23f707c8c805a
Jonathan Baraza <jbaraza46@gmail.com>
2022-05-24 13:28:51 +0300

    Added a new activity

---
+++ app/src/main/java/com/example/m_saidika/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.m_saidika;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+    public Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        btn=findViewById(R.id.btnProceed);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(WelcomeActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
