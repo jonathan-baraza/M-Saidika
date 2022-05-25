247de5b1163843ff83e4ac79a58cb22d499f7a73
Jonathan Baraza <jbaraza46@gmail.com>
2022-05-25 15:47:46 +0300

    Created Profile Activity

--- app/src/main/java/com/example/m_saidika/ProfileActivity.java
+++ app/src/main/java/com/example/m_saidika/ProfileActivity.java
@@ -10,5 +10,7 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //kitu tu
     }
 }
