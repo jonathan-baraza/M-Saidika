1a134380b7fa5ea93d7c7f525f59f24d4b675cd1
Jonathan Baraza <jbaraza46@gmail.com>
2022-05-25 15:52:53 +0300

    updated profile activity

--- app/src/main/java/com/example/m_saidika/ProfileActivity.java
+++ app/src/main/java/com/example/m_saidika/ProfileActivity.java
@@ -10,5 +10,6 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        //hello there
     }
 }
