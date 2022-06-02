f3ee6f7cc0bf21acc38725caee972fa78a8860b4
Jonathan Baraza <jbaraza46@gmail.com>
2022-06-02 12:45:36 +0300

    Included image library

--- app/src/main/java/com/example/m_saidika/EmergencyActivity.java
+++ app/src/main/java/com/example/m_saidika/EmergencyActivity.java
@@ -9,6 +9,6 @@ public class EmergencyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_emergency_activty);
+        setContentView(R.layout.activity_emergency);
     }
 }
