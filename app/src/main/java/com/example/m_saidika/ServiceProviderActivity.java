fa14fc7d34fd4b989dc42f4372f0fef08dc77c84
mikemutuma <mikemutuma664@gmail.com>
2022-06-03 15:27:44 +0300

    Created Food and Service Provider Activity

---
+++ app/src/main/java/com/example/m_saidika/ServiceProviderActivity.java
@@ -0,0 +1,14 @@
+package com.example.m_saidika;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServiceProviderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_provider);
+    }
+}
